55a5ea00b91263e07525a94cdfe3cc1f6f2cf0aa
Vanlocnong <95829802+Vanlocnong@users.noreply.github.com>
2022-11-12 21:01:32 +0700

    12/11
    12/11

---
+++ Lesson_1_the_first_C_program.c
@@ -0,0 +1,23 @@
+/*
+1 chuong trinh co ban gom 3 phan
+1.phan khai bao thu vien
+2.phan khai bao hang , bien...
+3.cac ham , phuong thuc
+*/
+
+//1. khai bao cac thu vien
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+
+//2. khai bao hang , bien
+
+
+
+//3. hang phuong thuc
+
+int main(){
+	printf("Hello World");
+	return 0;
+}
---
+++ Lesson_2_Data_type.c
@@ -0,0 +1,38 @@
+/*
+1 chuong trinh co ban gom 3 phan
+1.phan khai bao thu vien
+2.phan khai bao hang , bien...
+3.cac ham , phuong thuc
+*/
+
+//1. khai bao cac thu vien
+
+#include <stdio.h>			
+#include <stdlib.h>
+#include <conio.h>
+
+//Data_type
+
+/*
+1.khong co du lieu : void
+2. ky tu: char
+
+
+3. so nguyen : a. co dau : 3.a.1: short,int(-32k -> +32k)
+		 	               3.a.2: long (-2 -> +2 billion)
+		 	         
+		 	         
+               b. khong co dau: 3.b.1 : unsignedshort , unsigned int(2 -> 65k)
+   					            3.b.2 : unsignedlong(0 -> 4 billion) 
+   					
+   					
+4.float: float
+		 double
+		 long double
+*/
+
+int main(){
+		
+}
+
+
